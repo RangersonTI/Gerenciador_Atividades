c4c544a6f42b6595e370d45e22b8219db9ad6139
Rangerson Clemente <rangerson14@outlook.com>
2024-02-29 21:07:10 -0300

    update

--- tarefa/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class UsuarioForms(forms.Form):
-    nome = forms.CharField(max_length=100)
-    email = forms.CharField(max_length=100)
--- tarefa/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import listarUsuarios, cadastroUsuario, excluirUsuario, editarUsuario
-
-urlpatterns = [
-    path("listarUsuarios", listarUsuarios),
-    path("cadastroUsuario", cadastroUsuario),
-    path("excluirUsuario <int:id>", excluirUsuario),
-    path("editarUsuario <int:id>", editarUsuario),
-]
